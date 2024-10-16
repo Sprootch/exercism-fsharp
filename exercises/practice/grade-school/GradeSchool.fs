bc98d16b2a3d6fe86b7e50cc567a8f06950d7cb8
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-16 08:46:03 +0200

    Grade school

--- exercises/practice/grade-school/GradeSchool.fs
+++ exercises/practice/grade-school/GradeSchool.fs
@@ -2,10 +2,23 @@ module GradeSchool
 
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let grade (number: int) (school: School) : string list =
+    school |> Map.tryFind number |> Option.defaultValue []
 
-let roster (school: School): string list = failwith "You need to implement this function."
+let private exists student school =
+    school
+    |> Map.values
+    |> Seq.collect id
+    |> Seq.exists (fun name -> name = student)
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let add (student: string) (key: int) (school: School) : School =
+    if school |> exists student then
+        school
+    else
+        let students = grade key school
+        school |> Map.add key (student :: students |> List.sort)
+
+let roster (school: School) : string list =
+    school |> Map.toList |> List.collect snd |> List.distinct
--- exercises/practice/grade-school/GradeSchoolTests.fs
+++ exercises/practice/grade-school/GradeSchoolTests.fs
@@ -10,25 +10,25 @@ let ``Roster is empty when no student is added`` () =
     let school = empty
     roster school |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student is added to the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Aimee" 2
     roster school |> should equal ["Aimee"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Multiple students in the same grade are added to the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
         |> add "Paul" 2
     roster school |> should equal ["Blair"; "James"; "Paul"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student not added to same grade in the roster more than once`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
@@ -36,17 +36,17 @@ let ``Student not added to same grade in the roster more than once`` () =
         |> add "Paul" 2
     roster school |> should equal ["Blair"; "James"; "Paul"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Students in multiple grades are added to the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Chelsea" 3
         |> add "Logan" 7
     roster school |> should equal ["Chelsea"; "Logan"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student not added to multiple grades in the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
@@ -54,27 +54,27 @@ let ``Student not added to multiple grades in the roster`` () =
         |> add "Paul" 3
     roster school |> should equal ["Blair"; "James"; "Paul"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Students are sorted by grades in the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Jim" 3
         |> add "Peter" 2
         |> add "Anna" 1
     roster school |> should equal ["Anna"; "Peter"; "Jim"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Students are sorted by name in the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Peter" 2
         |> add "Zoe" 2
         |> add "Alex" 2
     roster school |> should equal ["Alex"; "Peter"; "Zoe"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Students are sorted by grades and then by name in the roster`` () =
-    let school = 
+    let school =
         empty
         |> add "Peter" 2
         |> add "Anna" 1
@@ -85,14 +85,14 @@ let ``Students are sorted by grades and then by name in the roster`` () =
         |> add "Charlie" 1
     roster school |> should equal ["Anna"; "Barb"; "Charlie"; "Alex"; "Peter"; "Zoe"; "Jim"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Grade is empty if no students in the roster`` () =
     let school = empty
     grade 1 school |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Grade is empty if no students in that grade`` () =
-    let school = 
+    let school =
         empty
         |> add "Peter" 2
         |> add "Zoe" 2
@@ -100,9 +100,9 @@ let ``Grade is empty if no students in that grade`` () =
         |> add "Jim" 3
     grade 1 school |> should be Empty
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student not added to same grade more than once`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
@@ -110,9 +110,9 @@ let ``Student not added to same grade more than once`` () =
         |> add "Paul" 2
     grade 2 school |> should equal ["Blair"; "James"; "Paul"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student not added to multiple grades`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
@@ -120,9 +120,9 @@ let ``Student not added to multiple grades`` () =
         |> add "Paul" 3
     grade 2 school |> should equal ["Blair"; "James"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Student not added to other grade for multiple grades`` () =
-    let school = 
+    let school =
         empty
         |> add "Blair" 2
         |> add "James" 2
@@ -130,12 +130,11 @@ let ``Student not added to other grade for multiple grades`` () =
         |> add "Paul" 3
     grade 3 school |> should equal ["Paul"]
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Students are sorted by name in a grade`` () =
-    let school = 
+    let school =
         empty
         |> add "Franklin" 5
         |> add "Bradley" 5
         |> add "Jeff" 1
     grade 5 school |> should equal ["Bradley"; "Franklin"]
-
