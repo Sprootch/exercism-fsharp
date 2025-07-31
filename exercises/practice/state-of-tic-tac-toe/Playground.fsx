020f2d6764986e97b015e08ead1abf64ae58b031
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2025-07-31 09:20:03 +0200

    TicTacToe (WIP)

---
+++ exercises/practice/state-of-tic-tac-toe/Playground.fsx
@@ -0,0 +1,32 @@
+﻿type EndGameState =
+    | Win
+    | Ongoing
+    | Draw
+
+type GameError =
+    | ConsecutiveMovesBySamePlayer
+    | WrongPlayerStarted
+    | MoveMadeAfterGameWasDone
+
+let board =
+        array2D [ ['X'; 'X'; 'O'];
+                  ['O'; 'X'; 'X'];
+                  ['X'; 'O'; 'O'] ]
+
+let rowWins =
+    [ for row in 0 .. ((board |> Array2D.length2) - 1) ->
+          board[row, *] |> Array.forall (fun cell -> cell = 'X')
+          || board[row, *] |> Array.forall (fun cell -> cell = 'O') ]
+
+let colWins =
+    [ for col in 0 .. ((board |> Array2D.length1) - 1) ->
+          board[*, col] |> Array.forall (fun cell -> cell = 'X')
+          || board[*, col] |> Array.forall (fun cell -> cell = 'O') ]
+
+let result = rowWins @ colWins |> List.countBy id
+
+match result with
+| [ (false, _); (true, j) ] -> if (j = 1) then Win else Ongoing
+| [ (true, j); (false, _) ] -> if (j = 1) then Win else Ongoing
+| [ (false, _) ] -> Draw
+| _ -> failwith "error"
--- exercises/practice/state-of-tic-tac-toe/StateOfTicTacToe.fs
+++ exercises/practice/state-of-tic-tac-toe/StateOfTicTacToe.fs
@@ -1,7 +1,30 @@
 module StateOfTicTacToe
 
-// TODO: define the 'EndGameState' type
-// TODO: define the 'GameError' type
+type EndGameState =
+    | Win
+    | Ongoing
+    | Draw
 
-let gameState board =
-    failwith "Please implement the 'gameState' function"
+type GameError =
+    | ConsecutiveMovesBySamePlayer
+    | WrongPlayerStarted
+    | MoveMadeAfterGameWasDone
+
+let gameState (board: char array2d) : Result<EndGameState, GameError> =
+    let rowWins =
+        [ for row in 0 .. ((board |> Array2D.length2) - 1) ->
+              board[row, *] |> Array.forall (fun cell -> cell = 'X')
+              || board[row, *] |> Array.forall (fun cell -> cell = 'O') ]
+
+    let colWins =
+        [ for col in 0 .. ((board |> Array2D.length1) - 1) ->
+              board[*, col] |> Array.forall (fun cell -> cell = 'X')
+              || board[*, col] |> Array.forall (fun cell -> cell = 'O') ]
+
+    let result = rowWins @ colWins |> List.countBy id
+
+    match result with
+    | [ (false, _); (true, j) ] -> if (j = 1) then Ok Win else Ok Ongoing
+    | [ (true, j); (false, _) ] -> if (j = 1) then Ok Win else Ok Ongoing
+    | [ (false, _) ] -> Ok Draw
+    | _ -> failwith "error"
